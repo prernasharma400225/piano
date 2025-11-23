0f2487d99f49e336c4b6bcdc3131322c39d32451
Prerna Sharma <prernasharma0925@gmail.com>
2025-11-22 16:37:14 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,457 @@
+var a1 = new Audio('./28.mp3')
+var a = new Audio('./29.mp3')
+var b = new Audio('./30.mp3')
+var c = new Audio('./31.mp3')
+var d = new Audio('./32.mp3')
+var e = new Audio('./33.mp3')
+var f = new Audio('./34.mp3')
+var g = new Audio('./35.mp3')
+var h = new Audio('./36.mp3')
+var i = new Audio('./37.mp3')
+var j = new Audio('./38.mp3')
+var k = new Audio('./39.mp3')
+var l = new Audio('./40.mp3')
+var m = new Audio('./41.mp3')
+var n = new Audio('./42.mp3')
+var o = new Audio('./43.mp3')
+var p = new Audio('./44.mp3')
+var q = new Audio('./45.mp3')
+var r = new Audio('./46.mp3')
+var s = new Audio('./47.mp3')
+var t = new Audio('./48.mp3')
+var u = new Audio('./49.mp3')
+var v = new Audio('./50.mp3')
+var w = new Audio('./51.mp3')
+var x = new Audio('./52.mp3')
+var y = new Audio('./53.mp3')
+var z = new Audio('./54.mp3')
+var aa = new Audio('./55.mp3')
+var bb = new Audio('./56.mp3')
+var cc = new Audio('./57.mp3')
+var dd = new Audio('./58.mp3')
+var ee = new Audio('./59.mp3')
+var ff = new Audio('./60.mp3')
+var gg = new Audio('./61.mp3')
+var hh = new Audio('./62.mp3')
+var ii = new Audio('./63.mp3')
+
+
+var body = document.body
+ var Z = document.querySelector('#z')
+ var U = document.querySelector('#u')
+ var T = document.querySelector('#t')
+ var S = document.querySelector('#s')
+ var R = document.querySelector('#r')
+ var Q = document.querySelector('#q')
+ var P = document.querySelector('#p')
+ var N = document.querySelector('#n')
+ var M = document.querySelector('#m')
+ var L = document.querySelector('#l')
+ var K = document.querySelector('#k')
+ var J = document.querySelector('#j')
+ var I = document.querySelector('#i')
+ var H = document.querySelector('#h')
+ var G = document.querySelector('#g')
+ var F = document.querySelector('#f')
+ var E = document.querySelector('#e')
+ var D = document.querySelector('#d')
+ var C = document.querySelector('#c')
+ var B = document.querySelector('#b')
+ var A = document.querySelector('#a')
+
+
+
+ var AA = document.querySelector('#aa')
+ var BB = document.querySelector('#bb')
+ var CC = document.querySelector('#cc')
+ var DD = document.querySelector('#dd')
+ var EE = document.querySelector('#ee')
+ var FF = document.querySelector('#ff')
+ var GG = document.querySelector('#gg')
+ var HH = document.querySelector('#hh')
+ var II = document.querySelector('#ii')
+ var JJ = document.querySelector('#jj')
+ var KK = document.querySelector('#kk')
+ var LL = document.querySelector('#ll')
+ var MM = document.querySelector('#mm')
+ var NN = document.querySelector('#nn')
+ var OO = document.querySelector('#oo')
+
+body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyZ"){
+           Z.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    Z.style.transform = "scale(1)";
+                },200)
+            a1.currentTime = 0;
+            a1.play()
+        }        
+ })
+
+ body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyX"){
+           U.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    U.style.transform = "scale(1)";
+                },200)
+            a.currentTime = 0;
+            a.play()
+        }        
+ })
+ body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyC"){
+           T.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    T.style.transform = "scale(1)";
+                },200)
+            b.currentTime = 0;
+            b.play()
+        }        
+ })
+ 
+ 
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyQ"){
+           S.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    S.style.transform = "scale(1)";
+                },200)
+            c.currentTime = 0;
+            c.play()
+        }        
+ })
+
+
+//  NOTDONE
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyW"){
+           R.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    R.style.transform = "scale(1)";
+                },200)
+            s.currentTime = 0;
+            s.play()
+        }        
+ })
+ 
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyE"){
+           Q.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    Q.style.transform = "scale(1)";
+                },200)
+            t.currentTime = 0;
+            t.play()
+        }        
+ })
+ 
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyR"){
+           P.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    P.style.transform = "scale(1)";
+                },200)
+            u.currentTime = 0;
+            u.play()
+        }        
+ })
+ 
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyT"){
+           N.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    N.style.transform = "scale(1)";
+                },200)
+            v.currentTime = 0;
+            v.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyY"){
+           M.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    M.style.transform = "scale(1)";
+                },200)
+            w.currentTime = 0;
+            w.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyU"){
+           L.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    L.style.transform = "scale(1)";
+                },200)
+            x.currentTime = 0;
+            x.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyI"){
+           K.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    K.style.transform = "scale(1)";
+                },200)
+            y.currentTime = 0;
+            y.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyO"){
+           J.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    J.style.transform = "scale(1)";
+                },200)
+           z.currentTime = 0;
+            z.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyP"){
+           I.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    I.style.transform = "scale(1)";
+                },200)
+            aa.currentTime = 0;
+            aa.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyM"){
+           H.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    H.style.transform = "scale(1)";
+                },200)
+            bb.currentTime = 0;
+            bb.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyN"){
+           G.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    G.style.transform = "scale(1)";
+                },200)
+            cc.currentTime = 0;
+            cc.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyB"){
+           F.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    F.style.transform = "scale(1)";
+                },200)
+            dd.currentTime = 0;
+            dd.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyV"){
+           E.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    E.style.transform = "scale(1)";
+                },200)
+            ee.currentTime = 0;
+            ee.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "BracketLeft"){
+           D.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    D.style.transform = "scale(1)";
+                },200)
+            ff.currentTime = 0;
+            ff.play()
+        }        
+ })
+  body.addEventListener('keydown',function(dets){
+        if(dets.code == "BracketRight"){
+           C.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    C.style.transform = "scale(1)";
+                },200)
+            gg.currentTime = 0;
+            gg.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Period"){
+           B.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    B.style.transform = "scale(1)";
+                },200)
+            hh.currentTime = 0;
+            hh.play()
+        }        
+ })
+ 
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Comma"){
+           A.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    A.style.transform = "scale(1)";
+                },200)
+            ii.currentTime = 0;
+            ii.play()
+        }        
+ })
+ 
+// black
+
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyS"){
+           AA.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    AA.style.transform = "scale(1)";
+                },200)
+            j.currentTime = 0;
+            j.play()
+        }        
+ })
+
+
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyD"){
+           BB.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    BB.style.transform = "scale(1)";
+                },200)
+            k.currentTime = 0;
+            k.play()
+        }        
+ })
+ 
+
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit2"){
+           CC.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    CC.style.transform = "scale(1)";
+                },200)
+            k.currentTime = 0;
+            k.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit3"){
+           DD.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    DD.style.transform = "scale(1)";
+                },200)
+            l.currentTime = 0;
+            l.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit4"){
+           EE.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    EE.style.transform = "scale(1)";
+                },200)
+            m.currentTime = 0;
+            m.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit6"){
+           FF.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    FF.style.transform = "scale(1)";
+                },200)
+            n.currentTime = 0;
+            n.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit7"){
+           GG.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    GG.style.transform = "scale(1)";
+                },200)
+            o.currentTime = 0;
+            o.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit8"){
+           HH.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    HH.style.transform = "scale(1)";
+                },200)
+            p.currentTime = 0;
+            p.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "Digit9"){
+          II.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    II.style.transform = "scale(1)";
+                },200)
+            q.currentTime = 0;
+            q.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyF"){
+           JJ.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    JJ.style.transform = "scale(1)";
+                },200)
+           r.currentTime = 0;
+            r.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyG"){
+           KK.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    KK.style.transform = "scale(1)";
+                },200)
+           s.currentTime = 0;
+            s.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyH"){
+           LL.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    LL.style.transform = "scale(1)";
+                },200)
+            t.currentTime = 0;
+            t.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyJ"){
+           MM.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                   MM.style.transform = "scale(1)";
+                },200)
+           u.currentTime = 0;
+            u.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyK"){
+           NN.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    NN.style.transform = "scale(1)";
+                },200)
+            v.currentTime = 0;
+            v.play()
+        }        
+ })
+   body.addEventListener('keydown',function(dets){
+        if(dets.code == "KeyL"){
+           OO.style.transform = "scale(0.95)";
+                setTimeout(function(){
+                    OO.style.transform = "scale(1)";
+                },200)
+           w.currentTime = 0;
+            w.play()
+        }        
+ })
+ 
